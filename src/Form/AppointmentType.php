e35470a105a65721be427c652f3b6b4321f62698
Bahloul <hitch95@protonmail.com>
2025-01-16 17:16:28 +0100

    handling form & submitting in Symfony

---
+++ src/Form/AppointmentType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Appointment;
+use App\Entity\Skill;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AppointmentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('email', EmailType::class)
+            ->add('phone', TelType::class)
+            ->add('startAt')
+            ->add('message')
+            ->add('skill', EntityType::class, [
+                'class' => Skill::class,
+                'choice_label' => 'name',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Appointment::class,
+        ]);
+    }
+}
---
+++ src/Form/SkillType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Doctor;
+use App\Entity\Skill;
+use App\Entity\Appointment;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SkillType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', options: [
+                'label' => 'Compétence',
+                'attr' => [
+                    'placeholder' => 'Saisir une compétence',
+                ],
+            ])
+        ;  
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Skill::class,
+        ]);
+    }
+}
