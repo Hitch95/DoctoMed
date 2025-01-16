9e67c2d1b76218dacd722b12356476b8a936503b
Bahloul <hitch95@protonmail.com>
2025-01-16 15:31:17 +0100

    try twig component for the opening hours

--- src/Controller/HealthcareCenterController.php
+++ src/Controller/HealthcareCenterController.php
@@ -2,20 +2,26 @@
 
 namespace App\Controller;
 
-use App\Entity\HealthcareCenter;
 use App\Repository\HealthcareCenterRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/healthcarecenter', name: 'app_healthcarecenter_')]
-
 final class HealthcareCenterController extends AbstractController
 {
     #[Route('/{slug}', name: 'show')]
-    public function show(string $slug, HealthcareCenterRepository $repository): Response
+    public function show(string $slug, HealthcareCenterRepository $repository, RequestStack $requestStack): Response
     {
         $healthcareCenter = $repository->findOneWithDoctors($slug);
+
+        if ($healthcareCenter === null) {
+            throw $this->createNotFoundException();
+        }
+
+        $requestStack->getSession()->set('healthcare_center_id', $healthcareCenter->getId());
+
         return $this->render('healthcare_center/show.html.twig', [
             'healthcare_center' => $healthcareCenter,
         ]);
