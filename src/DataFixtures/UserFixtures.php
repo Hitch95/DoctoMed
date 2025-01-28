8a04c132c341477efa5fe10aef67e9df9436b96e
Bahloul <hitch95@protonmail.com>
2025-01-28 23:56:41 +0100

    Task One - Add placeholder image for doctor without a photo

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -10,7 +10,9 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 class UserFixtures extends Fixture
 {
 
-    public function __construct(private UserPasswordHasherInterface $hasher) {}
+    public function __construct(private readonly UserPasswordHasherInterface $hasher)
+    {
+    }
 
     public function load(ObjectManager $manager): void
     {
