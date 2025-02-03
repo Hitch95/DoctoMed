80ad4d443676317ca1dbd38cf8f6c7073ef2ceb1
Hitch95 <bahloul.condorcet@gmail.com>
2025-02-03 11:39:04 +0100

    api plateform setup

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,6 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
